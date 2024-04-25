846cb7403ea7367f64e4f17e8bb921e83332227e
Firdavs28033 <burgutlochin11@gmail.com>
2024-04-25 08:40:57 +0500

    CSRF  allowed_host

--- omborxona/settings.py
+++ omborxona/settings.py
@@ -75,7 +75,17 @@ MIDDLEWARE = [
 
 
 CORS_ORIGIN_ALLOW_ALL = True
-
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 ROOT_URLCONF = 'omborxona.urls'
 
 TEMPLATES = [
